cd452d35d6f5b0230aba891bcf0bb9e76a1868c8
fufulouteur <manzodqb@icloud.com>
2023-09-09 10:10:38 +0000

    fini

---
+++ ft_str_is_lowercase.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ft_str_is_lowercase(char *str) {
+  if (str == NULL)
+    return 0;
+
+  if (*str == '\0')
+    return 1;
+
+  while (*str != '\0') {
+    if (*str < 'a' || *str > 'z')
+      return 0;
+    str++;
+  }
+  return 1;
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int ft_str_is_lowercase(char *str);
+
+int main() {
+  printf("%i\n", ft_str_is_lowercase("coucou"));
+  return 0;
+}
